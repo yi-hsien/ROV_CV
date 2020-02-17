65727e3527a45664fcb42e3d3dd592e7a13f2f85
Sammy <sumezawa@protonmail.com>
2020-02-17 14:47:19 -0800

    conversion of images to arrays

--- 3.4_Photomosaic/image_stitch_test1.py
+++ 3.4_Photomosaic/image_stitch_test1.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import glob
 import matplotlib.pyplot as plt
 
 # perform colour dominant tests
@@ -9,4 +10,29 @@ import matplotlib.pyplot as plt
 # second image with 1 long colour is bottom,
 # bottom box's long colour touches the matching side of the top box
 
-# get two small boxes, match to correct side of bottom 
+# get two small boxes, match to correct side of bottom box
+# align remaining long box to the right of the right small box
+
+
+"""
+    convert each of the five faces of the box into arrays 
+"""
+
+folders = glob.glob('*')
+print(folders)
+imagenames_list = []
+for f in folders:
+    if '.png' in f:
+        imagenames_list.append(f)
+
+read_images = []
+print(imagenames_list)
+
+for image in imagenames_list:
+    img = cv2.imread(image)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    read_images.append(img)
+
+"""
+
+"""
